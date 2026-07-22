d8e09ab4986f0168775ed9defd1e3b38e5ea4107
Jacques Potgieter <jgp@braemail.us>
2026-07-21 20:54:26 -0400

    feat(oee): named queries + BlueRidge.Oee.Downtime python wrappers (scope resolve + CRUD)

---
+++ ignition/projects/Core/ignition/named-query/oee/DowntimeEvent_RecordHistorical/query.sql
@@ -0,0 +1,8 @@
+EXEC Oee.DowntimeEvent_RecordHistorical
+    @ScopeLocationId      = :scopeLocationId,
+    @StartedAtEt          = :startedAtEt,
+    @EndedAtEt            = :endedAtEt,
+    @DowntimeReasonCodeId = :downtimeReasonCodeId,
+    @Remarks              = :remarks,
+    @AppUserId            = :appUserId,
+    @TerminalLocationId   = :terminalLocationId
